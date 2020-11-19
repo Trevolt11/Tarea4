f1cbbae3320fc80e9d01dbeea5937cc60f0fbe5b
Robert Caraballo <62614866+Trevolt11@users.noreply.github.com>
2020-11-19 14:21:42 -0400

    .

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el nombre: ");
+            string nombre_Res = (Console.ReadLine()); 
+            Singleton_class Nombre1 = Singleton_class.Nombre(nombre_Res);// Se le manda el dato registrado y crea una instancia
+            Console.WriteLine(Nombre1.nombre); //imprimer el valor almacenado de la instancia
+
+
+
+
+            Console.WriteLine("Digite otro nombre: ");
+            string nombre_Res1 = (Console.ReadLine()); 
+
+            Singleton_class Nombre2 = Singleton_class.Nombre(nombre_Res1);// Se le manda el dato registrado, pero no crea otra instancia por que ya se una con el dato anterior 
+            Console.WriteLine(Nombre2.nombre); //imprimer el valor almacenado de la instancia 
+
+
+            Console.WriteLine("Aqui imprime el primer valor que registramos, ya que no crea otra instancia sino que usa la que ya creo anterior mente con el valor ingresado" +
+                "ya que no crea otra instancia por que ya existe una.");
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton_class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton_class
+    {
+        private static Singleton_class registro = null;
+
+        public String nombre = "";
+
+        public String h;
+
+        protected Singleton_class(String h)
+        {
+            nombre = "El nombre almacenado es: " + h + "\n";
+        }
+
+        public static Singleton_class Nombre(String f)
+        {
+                if (registro == null) //verifica la existencia de la instancia
+                    registro = new Singleton_class(f);
+                return registro;
+        }
+    }
+}
