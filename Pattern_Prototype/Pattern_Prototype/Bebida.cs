7d105f68d58bd62b25333c930134a86706ae0d8b
Robert Caraballo <62614866+Trevolt11@users.noreply.github.com>
2020-11-20 12:40:03 -0400

    .

---
+++ Pattern_Prototype/Pattern_Prototype/Bebida.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern_Prototype
+{
+    class Bebida : ICloneable
+    {
+
+        public string nombre { get; set; }
+        public string grado_alcohol{ get; set; }
+        public int precio { get; set; }
+
+        public Info infor { get; set; }
+
+        public class Info
+        {
+            public string fecha_inventario { get; set; }
+            public string Color { get; set; }
+        }
+
+        public object Clone()
+        {
+            Bebida clonar = this.MemberwiseClone() as Bebida;
+
+            clonar.infor = new Info();
+            clonar.infor.fecha_inventario = infor.fecha_inventario;
+            clonar.infor.Color = infor.Color;
+            return clonar;
+        }
+
+        public override string ToString()
+        {
+            return "\n Nombre: " + nombre +"\n Precio: "+ precio +"\n Grado de alcohol: "+ grado_alcohol;
+        }
+    }
+}
---
+++ Pattern_Prototype/Pattern_Prototype/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Pattern_Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = DateTime.Now;
+            string v = date1.ToString("yyyy/MM/dd");
+            
+
+            Bebida bebida_ins = new Bebida() {nombre = "Ron Barcelo", grado_alcohol = 20 + "%", precio = 1230 };
+
+
+            bebida_ins.infor = new Bebida.Info();
+
+            Console.WriteLine("Original ");
+
+            Console.WriteLine("" + bebida_ins.infor.fecha_inventario);
+            Console.WriteLine(bebida_ins.ToString());
+            bebida_ins.infor.fecha_inventario = "2020/10/29";
+            Console.WriteLine(" Fecha que fue agregada la bebida: " + bebida_ins.infor.fecha_inventario + "");
+            bebida_ins.infor.Color = "Blanco";
+            Console.WriteLine(" Color de la bebida: " + bebida_ins.infor.Color + "\n");
+
+
+
+
+            Console.WriteLine("Clon \n");
+
+            Bebida bebida_insClone = bebida_ins.Clone() as Bebida;
+            bebida_insClone.infor.Color = "Rojo vino";
+            bebida_insClone.precio = 1500;
+            bebida_insClone.infor.fecha_inventario = v;
+            
+
+            Console.WriteLine(" Nombre clon : " + bebida_insClone.nombre);
+            Console.WriteLine(" Precio clon: " + bebida_insClone.precio);
+            Console.WriteLine(" Grado de alcohol clon: " + bebida_insClone.grado_alcohol);
+            Console.WriteLine(" Fecha que fue agregada la bebida clon: " + bebida_insClone.infor.fecha_inventario);
+            Console.WriteLine(" Color del clon: " + bebida_insClone.infor.Color);
+            
+            
+
+
+
+        }
+    }
+}
